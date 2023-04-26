9cb56151860ae79a633b95e5d87b1c27e7744188
rootAkash <mr.akashraut@gmail.com>
2023-04-27 00:56:20 +0530

    added buttons for entering prompts

--- pages/ChatDataCollector.py
+++ pages/ChatDataCollector.py
@@ -1,5 +1,24 @@
 import streamlit as st
 
-prompt_press = st.button("Enter new prompts!")
-continue_press = st.button("Continue from an existing chat!")
+if "chat_type" not in st.session_state:
+        st.session_state["chat_type"]=None
+if st.session_state["chat_type"] is None:
+    prompt_press = st.button("Enter new prompts!")
+    continue_press = st.button("Continue from an existing chat!")
+
+    if prompt_press:
+        st.session_state["chat_type"] = "Prompt"
+    if continue_press:
+        st.session_state["chat_type"] = "Continue"
+#print(st.session_state["chat_type"])
+else:
+    if st.session_state["chat_type"]  == "Prompt":
+        text  = st.text_input('Enter intial Prompt')
+        if text : 
+            print(text)
+    
+    restart = st.button("Restart")
+    if restart:
+        st.session_state["chat_type"]=None
+
 
